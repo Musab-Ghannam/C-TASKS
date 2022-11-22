df03c731cd63ae0cca2cf781a84667e3e99fd721
Musab-Ghannam <115452464+Musab-Ghannam@users.noreply.github.com>
2022-11-22 05:24:47 -0800

    22 11 2022

--- 21 11 2022/tasklast21 11 22/tasklast21 11 22/Program.cs
+++ 21 11 2022/tasklast21 11 22/tasklast21 11 22/Program.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace task21_11_22
+namespace tasklast21_11_22
 {
     internal class Program
     {
         static void Main(string[] args)
-            
-            
         {
-
             Console.WriteLine("ENTER TWO NUMBERS");
 
             int x = Convert.ToInt16(Console.ReadLine());
@@ -35,7 +32,7 @@ namespace task21_11_22
 
             int negative = Convert.ToInt16(Console.ReadLine());
 
-            if(negative <0)
+            if (negative < 0)
             {
 
                 Console.WriteLine("-");
@@ -54,14 +51,15 @@ namespace task21_11_22
             int w = 2;
             int q = 8;
 
-            if(sort>w&& w > q)
+            if (sort > w && w > q)
             {
 
-                Console.WriteLine(sort+"  " +w+" "+q);
-            }else if(sort >q && q > w)
+                Console.WriteLine(sort + "  " + w + " " + q);
+            }
+            else if (sort > q && q > w)
             {
 
-                Console.WriteLine(sort + "  " +q+ " " + w);
+                Console.WriteLine(sort + "  " + q + " " + w);
 
             }
             else if (w > sort && sort > q)
@@ -76,10 +74,10 @@ namespace task21_11_22
                 Console.WriteLine(w + "  " + q + " " + sort);
 
             }
-            else if (q > w&& w> sort)
+            else if (q > w && w > sort)
             {
-                 
-                Console.WriteLine(q+ "  " + w + " " + sort);
+
+                Console.WriteLine(q + "  " + w + " " + sort);
 
             }
             else if (q > sort && sort > w)
@@ -94,18 +92,19 @@ namespace task21_11_22
             Console.WriteLine("\n");
 
 
-            int a = 3; int b=4; int c=5; int d = 100 ,r=500;
+            int a = 3; int b = 4; int c = 5; int d = 100, r = 500;
 
 
-            if (a > b && a > c && a > d&& a>r)
+            if (a > b && a > c && a > d && a > r)
             {
                 Console.WriteLine(a);
-            }else if(b> a && b > c && b > d && b > r)
+            }
+            else if (b > a && b > c && b > d && b > r)
             {
 
                 Console.WriteLine(b);
             }
-            else if (c > a && c > b && c> d && c > r)
+            else if (c > a && c > b && c > d && c > r)
             {
 
                 Console.WriteLine(c);
@@ -113,7 +112,7 @@ namespace task21_11_22
 
 
 
-              else if (d > a && d > b && d> r&&d>c)
+            else if (d > a && d > b && d > r && d > c)
             {
 
                 Console.WriteLine(d);
@@ -128,30 +127,33 @@ namespace task21_11_22
 
             Console.WriteLine("\n");
 
-          float kilometers = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine(kilometers * 0.62137119);
+            float kilometers = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(kilometers * 0.62137119 + "mph");
 
             Console.WriteLine("\n");
 
-           int hours = Convert.ToInt16(Console.ReadLine());
+            int hours = Convert.ToInt16(Console.ReadLine());
             int minutes = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine(hours*60+minutes + "minutes");
+            Console.WriteLine(hours * 60 + minutes + "minutes");
 
 
             int totalhours = Convert.ToInt16(Console.ReadLine());
 
-            Console.WriteLine(totalhours/60 +"hours"+ ","+ totalhours%60 + "minutes");
+            Console.WriteLine(totalhours / 60 + "hours" + "," + totalhours % 60 + "minutes");
 
 
             Console.WriteLine("\n");
 
             string[] equal = { "ahmad", "mohammad", "rami", "khalid" };
 
-            Console.WriteLine(equal[0].Substring(0,2));
+            Console.WriteLine(equal[0].Substring(0, 2));
             Console.WriteLine(equal[1].Substring(0, 2));
             Console.WriteLine(equal[2].Substring(0, 2));
             Console.WriteLine(equal[3].Substring(0, 2));
-        }
 
+
+
+
+        }
     }
 }
---
+++ taskcsharp22 11 2022/taskcsharp22 11 2022/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace taskcsharp22_11_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CORRECT THE SYNTAX");
+
+            int[] ARR = { 1, 7, 9, 45 };
+
+            string[] ARR2 = { "str", "alex", "moh" };
+
+            string[] ARR3 = { "the", "fox", "lazy", "dog" };
+
+            Console.WriteLine("\n");
+
+
+            string[] fruits = { "tomato", "banana", "watermelon" };
+
+            Console.WriteLine("the index of tomato" +"\t "+ Array.IndexOf (fruits, "tomato"));
+            Console.WriteLine("the index of banana" + "\t " + Array.IndexOf(fruits, "banana"));
+
+            Console.WriteLine("\n");
+            Console.WriteLine("task 3");
+            string[] favoritefood = {"mansaf","maqloubeh","al-musakhan","humos","falafel" };
+            string[] favoritesport = { "footballmatch", "tennis", "hockey", "boxing", "yoga" };
+            string[] favoritemovie = { "matilda", "home-alone", "jumanji", "parenty-trap", "hocus-pocus" };
+
+            foreach(string favourit in favoritefood) {
+
+                Console.WriteLine(favourit);
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("favoritesport");
+
+            for (int i=0;i<favoritesport.Length;i++)
+            {
+
+
+                Console.WriteLine(favoritesport[i]);
+            }
+
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("favoritemovie");
+
+            foreach (string favour in favoritemovie )
+            {
+
+                Console.WriteLine(favour);
+            }
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("task4");
+
+           string[] SUM =Console.ReadLine().Split(',');
+
+
+           
+            int total = 0;
+
+            for (int i= 0;i< SUM.Length; i++)
+            {
+
+                total += Convert.ToInt32(SUM[i]);
+              
+                
+
+            }
+            Console.WriteLine(total);
+
+
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("task 5");
+            int odd = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    odd += i;
+
+                 
+                    Console.Write(i+"  ");
+
+                 
+
+                }
+              
+
+            }
+            Console.WriteLine("\n the sum of odd number is"+odd + "\t");
+
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("task6");
+
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("\t");
+
+                for(int o = 4; o > i; o--)
+                {
+                    Console.Write(" ");
+                }
+                for(int j=0; j<=i; j++)
+                {
+                  
+
+                    Console.Write("* ");
+
+                }
+
+
+            }
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("task7");
+
+            int tot = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("\t");
+              
+
+                
+                for (int j = 0; j <= i; j++)
+                {
+                    tot += 1;
+                    Console.Write(tot);
+
+                }
+
+
+            }
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("task8");
+            Console.WriteLine("\n");
+          double group = 0;
+            double fin = 0;
+           string[] avg = Console.ReadLine().Split(',');
+
+
+            for(int i = 0; i < avg.Length; i++)
+
+            {
+              
+                       for(int j=0; j<=i; j++)
+                {
+                    if (avg[j] == avg[i])
+                    {
+
+                        continue;
+                    }
+
+
+                   
+
+                }
+
+                group += Convert.ToDouble(avg[i]);
+
+
+
+
+
+
+
+
+            }
+            fin = group / avg.Length;
+
+            Console.WriteLine(fin);
+        }
+
+
+    }
+}
