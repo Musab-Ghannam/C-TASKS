ae6b8825ff62b296b18812577238cdad1f79135e
Musab-Ghannam <115452464+Musab-Ghannam@users.noreply.github.com>
2022-11-30 15:07:07 -0800

    30 11

---
+++ cal30nov/cal.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="cal.aspx.cs" Inherits="cal30nov.cal" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+       
+        <div>
+            <asp:Label runat="server" Text="Number1"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+
+            <asp:Label ID="Label1" runat="server" Text="Number2"></asp:Label>
+               <asp:TextBox ID="TextBox2" runat="server" style="width: 168px"></asp:TextBox>
+          
+        
+        <asp:Button ID="Buttonsum" runat="server" Text="+" OnClick="Buttonsum_Click" />
+
+        <asp:Button ID="Buttonsub" runat="server" Text="-" OnClick="Buttonsub_Click" />
+        <asp:Button ID="ButtonMul" runat="server" Text="*" OnClick="ButtonMul_Click" />
+        <asp:Button ID="ButtonDiv" runat="server" Text="/" OnClick="ButtonDiv_Click" />
+
+            </div>
+
+        <div id="result"><br />
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            <asp:TextBox ID="TextResult" runat="server"></asp:TextBox>
+            <asp:Button ID="Clear" runat="server" Text="Clear" OnClick="Clear_Click" />
+
+
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ cal30nov/cal.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cal30nov
+{
+    public partial class cal : System.Web.UI.Page
+    {
+        protected void Buttonsum_Click(object sender, EventArgs e)
+        {
+            int Sum = Convert.ToInt32(TextBox1.Text) + Convert.ToInt32(TextBox2.Text);
+            TextResult.Text = Convert.ToString(Sum);
+        }
+
+        protected void Buttonsub_Click(object sender, EventArgs e)
+        {
+            int Minus = Int32.Parse(TextBox1.Text) - Int32.Parse(TextBox2.Text);
+            TextResult.Text = Convert.ToString(Minus);
+        }
+
+        protected void ButtonMul_Click(object sender, EventArgs e)
+        {
+            int Mul = Int32.Parse(TextBox1.Text) * Int32.Parse(TextBox2.Text);
+            TextResult.Text = Convert.ToString(Mul);
+        }
+
+        protected void ButtonDiv_Click(object sender, EventArgs e)
+        {
+            int Div = Int32.Parse(TextBox1.Text) / Int32.Parse(TextBox2.Text);
+            TextResult.Text = Convert.ToString(Div);
+
+        }
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextResult.Text = "";
+        }
+    }
+}
