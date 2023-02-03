5b5c8d6aecb311f598d73d5070de64142102be7f
Musab-Ghannam <115452464+Musab-Ghannam@users.noreply.github.com>
2023-02-03 06:33:52 -0800

    2 3 2023

---
+++ problem solving3-2-2023/problem solving3-2-2023/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_solving3_2_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for(int i=1;i<=10;i++)
+            //{
+
+
+
+            //    Console.Write(i+" ");
+            //}
+
+            //int sum=0;
+
+            //for(int i= 1; i <= 10; i++)
+            //{
+            //    sum += i;
+
+            //}
+            //Console.Write(sum);
+
+            //Console.Write("\ntask 3\n");
+
+            //for (int i = 1; i <= 7; i++)
+            //{
+            //    Console.Write(i + " ");
+            //    sum += i;
+
+            //}
+            //Console.Write("the sum of theses 7 numbers is: "+sum);
+
+            // Console.Write("\ntask 4\n\n\n\nplease enter ten numbers");
+
+            // string[]a= Console.ReadLine().Split(' ');
+            //double s = 0;
+            // int count = 0;
+            // for(int i=0;i<a.Length;i++)
+            // {
+            //     s +=Convert.ToInt32( a[i]);
+
+            //     count++;
+            // }
+
+            // Console.Write("the sum of ten numbers is :"+s+"\n\n");
+
+            // Console.Write("the average of ten numbers is :" + (s /count) +"\n\n");
+
+            //Console.Write("\ntask 4\n\n\n\nplease enter ten numbers");
+
+            //string[]a=Console.ReadLine().Split(' ');
+
+            //for(int i = 0; i <= a.Length; i++)
+            //{
+            //    Console.Write($"the number is {a[i]} and the cube is {Math.Pow(Convert.ToInt16(a[i]), 3)} \n\n");
+
+            //}
+            //Console.Write("\ntask 6\n\n\n\nplease enter ten numbers");
+
+            //int numb=Convert.ToInt32(Console.ReadLine());
+
+            //for(int i=1;i<=10;i++)
+            //{
+
+            //    Console.WriteLine($"the result of muliple {numb} with{i} is: "+i*numb+"\n\n\n\n");
+
+            //}
+
+
+            //**************************************
+
+            //Console.Write("task 7\n");
+            //int other = 1;
+            //for(int i=1;i<=10;i++)
+            //{
+
+            //    for (int j=1;j<=8;j++)
+            //    {
+
+            //        Console.Write($" {other} * {j}= {other*j}");
+
+
+            //    }
+            //    other++;
+
+            //    Console.WriteLine("\n");
+            //}
+
+
+            //**********************************************************************************
+
+
+
+            //Console.Write("task 8\n");
+            //int sumodd = 0;
+            //Console.Write("the oddd number are\n" );
+            //for (int i = 1; i <= 20; i++)
+            //{
+
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.Write($"{i}\t");
+            //        sumodd += i;
+            //    }
+
+
+
+            //}
+            //Console.WriteLine($"\nthe sum of odd numbers is {sumodd}");
+
+            //**********************************************************************************
+            //Console.Write("task 9\n");
+
+            //for(int i=1;i<=4;i++) { 
+            //Console.Write("\n");
+            //for(int j=1;j<=i;j++)
+            //    {
+
+
+            //        Console.Write("*");
+            //    }
+
+
+            //**********************************************************************************
+            //Console.Write("task 10\n");
+            //for(int i=1;i<=4;i++)
+            //{
+            //    Console.WriteLine("");
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j);
+
+            //    }
+
+
+            //}
+
+            //**********************************************************************************
+
+            //Console.Write("task 11\n");
+            //int countnumb = 1;
+            //for(int i=1;i<=4;i++)
+
+            //{ Console.WriteLine("");
+
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(countnumb);
+            //    }
+            //    countnumb++;
+            //}
+
+            //*******************************************************************************************************************
+            //Console.WriteLine("task 12");
+            //int countsp = 1;
+            //for(int i = 1; i <= 4; i++)
+            //{
+            //    Console.WriteLine("");
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(countsp);
+            //        countsp++;
+
+
+            //    }
+
+
+
+            //}
+
+            //*******************************************************************************************************************
+
+            //Console.WriteLine(" task 13");
+            //int counth = 1;
+            //for(int i=1;i<=4;i++) {
+            //    Console.WriteLine("");
+
+
+            //    for (int k = 3; k >= i; k--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int j=1;j<=i;j++)
+            //    {
+
+
+            //        Console.Write(counth+" ");
+
+            //        counth++;
+
+            //    }
+
+            //}
+
+
+            //*******************************************************************************************************************
+
+        }
+    }
+}
